aa335ca2cf72fe4db63a74412b7aca1e2fd4e0ba
Bryan <79615060+bmon1216@users.noreply.github.com>
2021-08-23 15:12:12 -0700

    Update and rename BasicMath to YourName.py

---
+++ UW_PythonProjects/Module01-PythonBasics/YourName.py
@@ -0,0 +1,16 @@
+'''
+Assignment 01 - Your Name
+Bryan Mon
+IT FDN 110 A Su 21: Foundations Of Programming: Python
+July-13-2021
+'''
+
+#Title: A Simple Test Script
+print('Please enter your first name, press ENTER when finished')
+firstName = input() #Take input from user and save it to a variable
+print('Please enter your last name, press ENTER when finished')
+lastName = input() #Take input from user and save it to a variable
+print('Hello ' + firstName + ' ' + lastName)
+input('press Enter to exit')
+
+print("Your Name is:", first, last)
